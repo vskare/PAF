7f3e5f78f9b42737c601b94d3a4593c56a26c386
vskare <vskare@pmfst.hr>
2025-03-17 19:47:09 +0100

    Vjezba_1 dovreseno

--- Vjezbe/Vjezbe_1/Zadatak5.py
+++ Vjezbe/Vjezbe_1/Zadatak5.py
@@ -1,6 +1,26 @@
 import matplotlib.pyplot as plt
+import numpy as np
 def jednadzba_pravca(x1,y1,x2,y2):
     k = (y2-y1)/(x2-x1)
     l = y1 - k*x1
     print("y = {}x + {}".format(k,l))
-jednadzba_pravca(1,2,3,4)
+
+x1 = float(input("Unesite x koordinatu prve tocke: "))
+y1 = float(input("Unesite y koordinatu prve tocke: "))
+x2 = float(input("Unesite x koordinatu druge tocke: "))
+y2 = float(input("Unesite y koordinatu druge tocke: "))
+jednadzba_pravca(x1,y1,x2,y2)
+plt.xlabel('x')
+plt.ylabel('y')
+k = (y2-y1)/(x2-x1)
+l = y1 - k*x1
+x_vrijednosti = np.linspace(x1,x2)
+y_vrijednosti = k*x_vrijednosti + l
+plt.plot(x_vrijednosti, y_vrijednosti)
+plt.plot([x1,x2], [y1,y2], 'o')
+spremanje = input("zelite li spremiti graf kao pdf ili ga prikazati u programu (pdf/program)")
+if spremanje == 'program':
+    plt.show()
+elif spremanje == 'pdf':
+    filename = input("unesite ime datoteke") +'.pdf'
+    plt.savefig(filename)
--- Vjezbe/Vjezbe_1/Zadatal4.py
+++ Vjezbe/Vjezbe_1/Zadatal4.py
@@ -2,4 +2,8 @@ def jednadzba_pravca(x1,y1,x2,y2):
     k = (y2-y1)/(x2-x1)
     l = y1 - k*x1
     print("y = {}x + {}".format(k,l))
-jednadzba_pravca(1,2,3,4)
+x1 = float(input("Unesite x koordinatu prve tocke: "))
+y1 = float(input("Unesite y koordinatu prve tocke: "))
+x2 = float(input("Unesite x koordinatu druge tocke: "))
+y2 = float(input("Unesite y koordinatu druge tocke: "))
+jednadzba_pravca(x1,y1,x2,y2)
